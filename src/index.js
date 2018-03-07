f8348a58a25b9dece1898f4ba9ad770606b00309
Mitch Robb <olordhux+github@gmail.com>
2018-03-06 23:33:11 -0800

    ignore clock_tick actions in redux devtools

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const sagaMiddleware = createSagaMiddleware();
 const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(sagaMiddleware))
+  composeEnhancers({
+    actionsBlacklist: ['CLOCK_TICK']
+  })(applyMiddleware(sagaMiddleware))
 );
 sagaMiddleware.run(rootSaga);
 
