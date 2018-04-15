9b73e647eac2068377b598bae5dafae1f094a3fb
Mitch Robb <olordhux+github@gmail.com>
2018-04-14 19:15:34 -0700

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+const env = require('veritone-dev-env');
+
+module.exports = Object.assign({}, env.eslintReact, {
+  plugins: env.eslintReact.plugins.concat('import'),
+  rules: Object.assign({}, env.eslintReact.rules, { 'import/order': 2 })
+});
