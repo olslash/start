20073a445ebf573664a822d9fc60018227483269
Mitch Robb <olordhux+github@gmail.com>
2018-09-03 14:06:28 -0700

    add icons to taskbar items

--- src/components/TaskBar/TaskBarItem.js
+++ src/components/TaskBar/TaskBarItem.js
@@ -2,10 +2,12 @@ import React from 'react';
 import p from 'prop-types';
 import cx from 'classnames';
 
+// import screenDoor from '../../../resources/selected-overlay-screen-inverse-40.svg';
+import icons from '../../../resources/icons'
 import ButtonBase from '../ButtonBase';
 import styles from './taskBarItem.scss';
 
-const TaskBarItem = ({ title, active, onClick }) => (
+const TaskBarItem = ({ title, active, onClick, icon }) => (
   <div className={styles.container}>
     <ButtonBase
       classes={{
@@ -15,8 +17,12 @@ const TaskBarItem = ({ title, active, onClick }) => (
       onClick={onClick}
       stopClickPropagation
     >
-      <div className={cx(styles.title, { [styles.active]: active })}>
-        {title}
+      <div
+        className={cx(styles.title, { [styles.active]: active })}
+
+      >
+        <img src={icons[icon]} />
+        <span>{title}</span>
       </div>
     </ButtonBase>
   </div>
@@ -24,6 +30,7 @@ const TaskBarItem = ({ title, active, onClick }) => (
 
 TaskBarItem.propTypes = {
   title: p.string,
+  icon: p.string.isRequired,
   active: p.bool,
   onClick: p.func
 };
