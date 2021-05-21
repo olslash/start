e7ac66546769637813c77db08d13eb10371fd05d
mitch <olordhux@gmail.com>
2021-05-21 00:31:11 -0700

    bump linter stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,21 +2,24 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
   ],
   parser: '@typescript-eslint/parser',
 
   plugins: ['import', '@typescript-eslint'],
   rules: {
+    'no-use-before-define': 0,
     'import/order': 2,
     'react/jsx-no-bind': 0,
     'react/prop-types': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
-    '@typescript-eslint/camelcase': 0
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    'no-unused-vars': 0,
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: 'detect',
+    },
+  },
 };
