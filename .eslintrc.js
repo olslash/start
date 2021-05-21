72d8fea94df3e3fa1618b6144c175116ffea730e
mitch <olordhux@gmail.com>
2021-05-21 01:48:25 -0700

    disable lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'react/jsx-no-bind': 0,
     'react/prop-types': 0,
     '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 0,
     '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/no-use-before-define': 0,
     'no-unused-vars': 0,
